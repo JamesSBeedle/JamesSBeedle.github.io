79091974f593c5a88ece1ca1adedd28e06c29200
JamesSBeedle <jamessbeedle@gmail.com>
2021-04-30 10:14:10 +0100

    removed content of projects file, moved porjects file to create a container

---
+++ src/containers/ProjectContainer.js
@@ -0,0 +1,26 @@
+import "./Project.css"
+import innHome from "../images/inn-home-page.png"
+import innAdd from "../images/inn-add-product.png"
+import innProduct from "../images/inn-product-page.png"
+// import Footer from './Footer'
+
+const Project = () => {
+
+    return(
+        <>
+           
+
+            <section class="project-container" id="projects">
+                <h1>Projects</h1>
+
+                
+                
+            </section>
+            
+
+          
+        </>
+    )
+}
+
+export default Project
