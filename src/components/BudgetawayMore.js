0ff5259e6d9bb00038698c7ce7fc4375061a880f
JamesSBeedle <jamessbeedle@gmail.com>
2021-05-07 16:54:59 +0100

    changed some of the description text, added scroll to budgetaway.

--- src/components/BudgetawayMore.js
+++ src/components/BudgetawayMore.js
@@ -25,7 +25,18 @@ const BudgetawayMore = () => {
             <section className="description-container">
 
             <p className="description-lg">
-                        Budgetaway was designed and developed by a team of CodeClan students with the aim of giving people working on a budget a way to select holiday destinations and flights. It allows the user to select a country using filters for language and region. It then displays information about the country and the user can then select a departure airport and an arrival airport to get flight information. 
+            Budgetaway was designed and developed by a team of CodeClan students with the aim of giving people working on a budget a way to select holiday destinations and flights. It allows the user to select a country using filters for language and region. It then displays information about the country and the user can select a departure airport and an arrival airport to get flight information. 
+                        <br></br>
+                        <br></br>
+                        The app also features a lucky dip button which will allow a user to randomly select any country in the world rather than using the filters or selecting a country directly.
+                        <br></br>
+                        <br />
+
+                        Any journey the user creates can be added to a wishlist of destinations to store for later use or deleted as appropriate.
+                        <br />
+                        <br/>
+
+                        The application also includes a side-bar budgeting calculator. This allows the user to enter their financial details and the journey cost and will then calculate how much they can save per month and, if they do so, how many months it will be before they can afford the trip.
                  
                
                     </p>
--- src/components/TryAtomicMore.js
+++ src/components/TryAtomicMore.js
@@ -27,7 +27,7 @@ const TryAtomicMore = () => {
 
             <section className="description-container">
                     <p className="description-lg">
-                    The try-atomic app was designed and developed as part of a Group Project. The aim of the team was to develop an app that would track a users carbon emissions and display their overall carbon footprint over the period of 1 month. It collects user inputted information on diet type (daily), transport (per journey) and enegry use (monthly) and calculates each inputs individual carbon emissions. It then displays the total emissions for the month and compares it to the average person.
+                    The try-atomic app was designed and developed as part of a Group Project. The aim of the team was to develop an app that would track a users carbon emissions and display their carbon footprint over the period of 1 month. It collects user inputted information on diet type (daily), transport (per journey) and enegry use (monthly) and calculates each inputs individual carbon emissions. It then displays the total emissions for the month and compares it to the average person.
                     </p>
             </section>
 
