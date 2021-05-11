39acd4c013b116a0c0151e9e7d8f9f0c236de633
JamesSBeedle <jamessbeedle@gmail.com>
2021-05-11 21:38:46 +0100

    added images to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import "./Home.css"
 import Footer from './Footer'
+import Java from "../images/Java.jpg"
+import react from "../images/React.jpg"
+import python from "../images/Python.jpg"
 
 const Home = () => {
     return(
@@ -12,6 +15,11 @@ const Home = () => {
                     <h2 className="profession">Software Developer</h2>
                     <div className="underline"></div>
                     <h2 className="portfolio">Portfolio </h2>
+                    <div className="image-container-home">
+                    <img className="small-img" src ={Java} alt = "Java logo"></img>
+                    <img className="small-img" src ={react} alt = "React logo"></img>
+                    <img className="small-img" src ={python} alt = "Python3 logo"></img>
+                    </div>
                 </section>  
                 <Footer/> 
             </>
